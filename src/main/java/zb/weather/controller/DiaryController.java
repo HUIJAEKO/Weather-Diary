7ac6988b6e012c42a76dd89b2ed03e6212c7b7f8
HUIJAEKO <xrhgmlwox@naver.com>
2025-01-27 14:00:26 +0900

    fix: change mapping for save

--- src/main/java/zb/weather/controller/DiaryController.java
+++ src/main/java/zb/weather/controller/DiaryController.java
@@ -2,10 +2,7 @@ package zb.weather.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import zb.weather.service.DiaryService;
 
 import java.time.LocalDate;
@@ -15,7 +12,7 @@ import java.time.LocalDate;
 public class DiaryController {
     private final DiaryService diaryService;
 
-    @GetMapping("/create/diary")
+    @PostMapping("/create/diary")
     void createDiary(
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
             @RequestBody String text){
